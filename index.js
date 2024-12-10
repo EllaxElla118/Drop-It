9b86c429dc1b3ef04ed4c9df52986ecc432f7aa8
EllaxElla118 <onasogaemmanuel02@gmail.com>
2024-12-10 21:37:28 +0100

    DropIt v1.0 Stable Release

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-var fileName = '';
 var imgPath = '';
 const extImgs = {
     pdf: 'resources/imgs/pdf.png',
@@ -10,13 +9,23 @@ const extImgs = {
 };
 let template;
 const error =  {
-    connClose: `Connection closed unexpectedly. Check your connection and reload the Page...`,
-}
+    connClose: `Connection closed unexpectedly. Check your connection and reload the Page...`
+};
 
 
 
 document.addEventListener("DOMContentLoaded", function() {
-  
+    /* Feature will be added in future release
+  document.querySelector('#webID').addEventListener('input', function() {
+      if(this.value.length === 6) {
+          setTimeout(function() {
+            swapTo(4);
+            joinWeb(this.value);
+          }, 1000)
+      }
+    });
+    */
+
   document.querySelector('.create').addEventListener('click', function() {
     const bodyArea = document.querySelector('.body_area');
     if (bodyArea.style.display === 'none' || bodyArea.style.display === '') {
@@ -25,16 +34,13 @@ document.addEventListener("DOMContentLoaded", function() {
       bodyArea.style.display = 'none';
     }
   });
-  
+
 });
 
 window.addEventListener("DOMContentLoaded", (event) => {
     template = document.querySelector('.icFile');
     document.getElementById('file').addEventListener('change', (event) => {
         idSelector();
-        file = event.target.files[0];
-        if(!file) return;
-        fileName = file.name;
     });
 });
 
@@ -48,12 +54,34 @@ function openFilePicker() {
     document.getElementById('file').click();
 }
 
-async function send() {
+function nfncts_close() {
+        document.querySelector('#id_div').style.display = 'none';
+        document.querySelector('.qrScanner').style.display = 'none';
+        document.querySelector('#overlay').style.display = 'none';
+}
+
+async function send(r) {
+    document.querySelector('#sBtn').innerHTML = "<img style='width: 18%' src='resources/imgs/spinner1.svg' />";
+    document.querySelector('#sBtn').disabled = 'true';
+    document.querySelector('#sBtn').style.backgroundColor = 'white';
+    document.querySelector('#sBtn').style.border = '2px solid green';
+    setTimeout(function() {
+        document.querySelector('#id_div').style.display = 'none';
+        document.querySelector('#overlay').style.display = 'none';
+        document.querySelector('#sBtn').innerHTML = "Send";
+        document.querySelector('#sBtn').disabled = 'false';
+        document.querySelector('#sBtn').style.backgroundColor = '#4CAF50';
+        document.querySelector('#sBtn').style.border = '2px solid white';
+    }, 1500);
     let chunkSize = 1024 * 50;
-    let fileID = Math.floor((10**5) + Math.random() * (9 * (10**5)));
    let id = document.getElementById('in_id').value;
-   let isLargeFile = file.size > 1024 * 1024 * 300;
-   console.log(isLargeFile);
+   const fileInput = document.querySelector('#file');
+   const files = r || fileInput.files;
+
+    for (let x = 0; x < files.length; x++) {
+        const file = files[x]; // Access the actual File object
+        let fileID = Math.floor((10**5) + Math.random() * (9 * (10**5)));
+        let isLargeFile = file.size > 1024 * 1024 * 300;
         splitFile(
             file,                      
             chunkSize,                     
@@ -61,7 +89,7 @@ async function send() {
                 const message = {
                     type: 'file',
                     targetClientId: id,
-                    fileName: fileName,
+                    fileName: file.name,
                     fileSize: file.size,
                     fileID: fileID,
                     sequencenumber: sequencenumber,
@@ -69,13 +97,25 @@ async function send() {
                 };
                 const msgJSON = JSON.stringify(message);
                 const metadataBuffer = new TextEncoder().encode(msgJSON + "\0");
-                let x = mergeBuffers(metadataBuffer.buffer, chunk);
-                ws.send(x);
+                let z = mergeBuffers(metadataBuffer.buffer, chunk);
+                ws.send(z);
+                th = Math.floor((sequencenumber * 100) * (Math.ceil(file.size / (1024 * 50))**-1));
+                document.querySelector('.sNote').innerHTML = files.length > 1 ? (`Sending ${files.length} files`) : (th === 100 ? '' : `${th}%`);
         });
+                
+                                                                                /* true
+            /*
+                if(files.length === 1) {
+                    document.querySelector('.sNote').innerHTML = th === 100 ? '<i>Sent...</i>' : th + '%';
+                } else {
+                    document.querySelector('.sNote').innerHTML = `Sending ${files.length} files`;
+                }
+*/
+    };
 }
 
 function idSelector() {
-    document.getElementById('id_div').style.display = 'grid';
+    document.getElementById('id_div').style.display = 'flex';
     document.getElementById('overlay').style.display = 'block';
 }
 
@@ -87,11 +127,12 @@ function incomingFile(servedfileName, id, w) {
     a.querySelector('.progressInner').setAttribute('id', id);
     a.querySelector('#imgFileType').src = imgPath;
     if(!w) {
-        a.querySelector('.dBtn').onclick = async function() {
-            this.src = 'resources/imgs/spinner1.svg'
-            await setTimeout(10000);
-            await deleteEntry(id); 
-            createFile(id, servedfileName);
+        a.querySelector('.dBtn').onclick = function() {
+            this.children[0].src = 'resources/imgs/spinner1.svg';
+            setTimeout(async function() {
+                await createFile(id, servedfileName);
+                deleteEntry(id); 
+            }, 1000);
         }
     } else{
         a.querySelector('.dBtn').onclick = async function() {
@@ -113,11 +154,15 @@ function incomingFile(servedfileName, id, w) {
 
 memory = [];
 
-    // 
+    // ws://127.0.0.1:2104 //
     let ws = new WebSocket('wss://don-m0rx.onrender.com/');
-    
+
     ws.onopen = () => { 
-        document.querySelector('.load_overlay').style.display = 'none';
+        if(document.readyState === 'loading') {
+            location.reload();
+            return;
+        }
+        document.querySelector('.load_overlay').style.display = 'none';     
    };
 
     ws.onerror = () => { 
@@ -126,8 +171,7 @@ memory = [];
    };
 
    ws.onclose = (event) => {
-        document.querySelector('.load_overlay').innerHTML = error.connClose;
-        document.querySelector('.load_overlay').style.display = 'flex';
+       throwError('wild', error.connClose);
     };
 
     let memory_block_2 = [];
@@ -143,8 +187,9 @@ memory = [];
                         speedTimer(r.fileID, true);
                     } else {  
                         document.getElementById(`${r.fileID}`).style.flex = cCalc/100;
-                        let t = document.getElementById(`${r.fileID}`).parentElement.parentElement
-                        t.querySelector('#speedTxt').innerText = speedTimer(r.fileID, false, r.sequencenumber);
+                        let t = document.getElementById(`${r.fileID}`).parentElement.parentElement;
+                        let f = speedTimer(r.fileID, false, r.sequencenumber);
+                        t.querySelector('#speedTxt').innerText = f != 'NaN kb/s' ? f : '-- b/s';
                         t.querySelector('#fileProgress').innerText = progessCalc(r.sequencenumber, r.fileSize);
                     }
                     if(cCalc === 100) {
@@ -156,11 +201,16 @@ memory = [];
                 });
         } else {  
             let msg = JSON.parse(message.data);
-            if(msg.type === 'id') {   document.getElementById('id').innerText = msg.message; }
+            if(msg.type === 'id') {   
+                document.getElementById('id').innerText = msg.message;                       
+                appendQR(msg.message);
+             }
             else if(msg.type === 'client_list') {
-                
+
             } else if(msg.type === 'Error') {
-                alert('App Crashed');
+                throwError('mild', msg.message);
+            } else if(msg.type === 'QRAccept') {
+                acceptedQR(msg.message);
             }     
     };
 };
@@ -240,5 +290,69 @@ function progessCalc(a,b) {
         while (b > t_1[i] && i < t_1.length - 1) {
             i++;
         }
-    return `${((a*1024*50)/t_1[i-1]).toFixed(0)}${t_2[i-1]}/${((b)/t_1[i-1]).toFixed(0)}${t_2[i-1]}`;
+    return `${((a*1024*50)/t_1[i-1]).toFixed(1)}${t_2[i-1]}/${((b)/t_1[i-1]).toFixed(1)}${t_2[i-1]}`;
 }
+
+function throwError(level = 'mild', error = 'An unknown Error occurred') {
+    if(level === 'wild') {
+        document.querySelector('.load_overlay').innerHTML = error;
+        document.querySelector('.load_overlay').style.display = 'flex';
+    } else {
+        alert(error);
+    }
+}
+
+async function appendQR(id) {
+    const qrCode = new QRCodeStyling({
+        width: 200,
+        height: 200,
+        type: "svg",
+        data: `{
+            "ifo": "Visit DropIt and Scan this QR with the DropIt Scanner to receive the file(s)",
+            "uuid": ${id}
+        }`,
+        dotsOptions: {
+            color: "green",
+            type: "rounded"
+        },
+        backgroundOptions: {
+            color: "white",
+        },
+        imageOptions: {
+            crossOrigin: "anonymous",
+            margin: 20
+        }
+    });
+    await qrCode.download();
+    document.querySelector('.qrImg').src = document.querySelector('#qr').src;
+    document.querySelector('#qr').remove();
+}
+
+function acceptedQR(s) {
+    document.querySelector('#in_id').value = s;
+    document.querySelector('#sBtn').click()
+}
+
+/* Feature will be added in future release 
+
+function toggleWebCont() {
+    let a = document.querySelector('.ird');
+    const currentVisibility = getComputedStyle(a).visibility;
+    const currentOpacity = getComputedStyle(a).opacity;
+    if (currentVisibility === 'visible' && currentOpacity === '1') {
+        a.style.opacity = '0';
+        a.style.visibility = 'hidden';
+    } else {
+        a.style.opacity = '1';
+        a.style.visibility = 'visible';
+    }
+}
+
+function swapTo(id) {
+    for(let i=1;i<3;i++) {
+        document.querySelector('.w' + i).style.display = 'none';
+    }
+    document.querySelector('.w' + id).style.display = 'flex';
+}
+
+*/
